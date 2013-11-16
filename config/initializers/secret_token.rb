9a773be4984231a5f5e5908d2e951588ef5cdbf0
Grant Cahill <grant.cahill@sv.cmu.edu>
2013-11-16 13:43:13 -0800

    updating secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SdspDataViz::Application.config.secret_token = '2cc11fe910d82d9e7c3e390ac157488d46c2dc1b2853d59462cf161ffb2d6708fba731a014c1ab6d516b31912c4219b1c981d72001c25bba118459ccbd3a92b3'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
